09b426dd22df9323f7f1cd729778aee8bbd5b103
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-08-07 20:24:08 +0300

    Create user password reset schema

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -20,7 +20,7 @@ from database import (
 from exceptions import BaseSecurityError
 from security.interfaces import JWTAuthManagerInterface
 
-from schemas.accounts import UserRegistrationResponseSchema, UserRegistrationRequestSchema
+from schemas.accounts import UserRegistrationResponseSchema, UserRegistrationRequestSchema, UserActivationRequestSchema, PasswordResetRequestSchema
 from security.passwords import hash_password
 
 router = APIRouter()
@@ -53,3 +53,52 @@ async def register(user: UserRegistrationRequestSchema, db: AsyncSession = Depen
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="An error occurred during user creation."
         )
+
+
+@router.post("/activate/", status_code=status.HTTP_200_OK)
+async def activate_user_account(request: UserActivationRequestSchema, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(
+        select(ActivationTokenModel)
+        .where(ActivationTokenModel.token == request.token)
+    )
+    activation_token = result.scalars().first()
+
+    if not activation_token or activation_token.expires_at < datetime.now():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid or expired activation token."
+        )
+
+    result = await db.execute(
+        select(UserModel)
+        .where(UserModel.email == request.email)
+    )
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid or expired activation token."
+        )
+
+    if user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User account is already active."
+        )
+
+    user.is_active = True
+    await db.delete(activation_token)
+    await db.commit()
+
+    return {"message": "User account activated successfully."}
+
+
+@router.post("/password-reset/request/",response_model=PasswordResetRequestSchema, status_code=status.HTTP_200_OK)
+async def reset_password(email: str, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(UserModel).where(UserModel.email == email))
+    db_user = result.scalars().first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    await db.commit()
+    return {"detail": "Password updated"}
