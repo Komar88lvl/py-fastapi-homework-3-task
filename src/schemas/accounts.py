cea4a1e75406f73502aeb6dc4e474bc6317dbc4f
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-08-07 14:58:12 +0300

    Create empty models

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -1,6 +1,44 @@
-from pydantic import BaseModel, EmailStr, field_validator
+from pydantic import BaseModel, EmailStr, field_validator, Field
 
 from database import accounts_validators
 
 
-# Write your code here
+class UserRegistrationRequestSchema(BaseModel):
+    email: EmailStr
+    password: str = Field(min_length=8, max_length=30)
+
+
+class UserRegistrationResponseSchema(BaseModel):
+    pass
+
+
+class UserActivationRequestSchema(BaseModel):
+    pass
+
+
+class MessageResponseSchema(BaseModel):
+    pass
+
+
+class PasswordResetRequestSchema(BaseModel):
+    pass
+
+
+class PasswordResetCompleteRequestSchema(BaseModel):
+    pass
+
+
+class UserLoginResponseSchema(BaseModel):
+    pass
+
+
+class UserLoginRequestSchema(BaseModel):
+    pass
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    pass
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    pass
